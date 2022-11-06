321bf921ea14f80d429301fee04f1f9b24056a37
Hua0720 <stella573021@gmail.com>
2022-11-06 14:02:07 +0800

    feat: add restaurants list final

---
+++ routes/modules/users.js
@@ -0,0 +1,10 @@
+// 引用 Express 與 Express 路由器
+const express = require('express')
+const router = express.Router()
+
+// 用get加入一條「登入表單頁面」的路由， 再用render匯出路由模組
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
+module.exports = router
