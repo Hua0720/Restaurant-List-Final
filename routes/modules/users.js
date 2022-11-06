90ef6cc9c42e68f936fe9862aff40146ef86f0e6
Hua0720 <stella573021@gmail.com>
2022-11-06 19:55:16 +0800

    feat: add bcrypt.js

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -8,6 +8,9 @@ const User = require('../../models/user')
 // 引用 passport
 const passport = require('passport')
 
+// 引用 bcrypt
+const bcrypt = require('bcryptjs')  // 載入套件
+
 // 用get加入一條「登入表單頁面」的路由， 再用render匯出路由模組
 router.get('/login', (req, res) => {
   res.render('login')
@@ -61,18 +64,19 @@ router.post('/register', (req, res) => {
         password,
         confirmPassword
       })
-    } else {
-      // 如果還沒註冊：寫入資料庫
-      return User.create({
+    }
+    // 如果還沒註冊：寫入資料庫
+    return bcrypt
+      .genSalt(10) // 產生「鹽」，並設定複雜度係數為 10
+      .then(salt => bcrypt.hash(password, salt)) // 為使用者密碼「加鹽」，產生雜湊值
+      .then(hash => User.create({
         name,
         email,
-        password
-      })
-        .then(() => res.redirect('/'))
-        .catch(err => console.log(err))
-    }
+        password: hash // 用雜湊值取代原本的使用者密碼
+      }))
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
   })
-    .catch(err => console.log(err))
 })
 
 // 設定登出路由
