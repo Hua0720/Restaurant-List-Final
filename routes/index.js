3b419f819e904e11b7139d06d096cdbce8020b16
Hua0720 <stella573021@gmail.com>
2022-11-06 14:04:55 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -11,8 +11,10 @@ const users = require('./modules/users')
 
 // 將網址結構符合 / 字串的 request 導向 home 模組 
 router.use('/', home)
+
 // 將網址結構符合 /restaurants 字串開頭的 request 導向 restaurants 模組 
 router.use('/restaurants', restaurants)
+
 // 將網址結構符合 /users 字串開頭的 request 導向 users 模組
 router.use('/users', users)
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const router = express.Router()
 
+
 // 用get加入一條「登入表單頁面」的路由， 再用render匯出路由模組
 router.get('/login', (req, res) => {
   res.render('login')
