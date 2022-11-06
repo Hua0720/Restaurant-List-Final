a10a7e9ef756daff1baa4f1b60e1354734434d2c
Hua0720 <stella573021@gmail.com>
2022-11-06 14:23:10 +0800

    feat: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,10 +2,18 @@
 const express = require('express')
 const router = express.Router()
 
-
 // 用get加入一條「登入表單頁面」的路由， 再用render匯出路由模組
 router.get('/login', (req, res) => {
   res.render('login')
+})
+
+// 用post加入一條「login登入進去」的路由
+router.post('/login', (req, res) => {
+})
+
+// 用get加入一條「註冊頁面」的路由， 再用render匯出路由模組
+router.get('/register', (req, res) => {
+  res.render('register')
 })
 
 module.exports = router
