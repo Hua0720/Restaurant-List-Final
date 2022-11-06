dd993b4163399971be6822e444bc521a27431e91
Hua0720 <stella573021@gmail.com>
2022-11-06 19:07:27 +0800

    feat: add relations between User and restaurants  models

--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -7,29 +7,33 @@ const Restaurant = require("../../models/Restaurant")
 
 // 新增餐廳頁面
 router.get("/new", (req, res) => {
-  res.render("new")
+  return res.render("new")
+})
+
+// 新增餐廳
+router.post('/', (req, res) => {
+  const userId = req.user._id
+  const { name, name_en, category, image, location, phone, google_map, rating, description } = req.body
+  return Restaurant.create({ name, name_en, category, image, location, phone, google_map, rating, description, userId })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 // 瀏覽特定餐廳
 router.get('/:id', (req, res) => {
-  const { id } = req.params
-  Restaurant.findById(id)
+  const userId = req.user._id
+  const _id = req.params.id
+  return Restaurant.findOne({ _id, userId })
     .lean()
     .then(restaurant => res.render("show", { restaurant }))
     .catch(err => console.log(err))
 })
 
-// 新增餐廳
-router.post("/", (req, res) => {
-  Restaurant.create(req.body)
-    .then(() => res.redirect("/"))
-    .catch(err => console.log(err))
-})
-
 // 編輯餐廳頁面
 router.get("/:id/edit", (req, res) => {
-  const { id } = req.params
-  Restaurant.findById(id)
+  const userId = req.user._id
+  const _id = req.params.id
+  return Restaurant.findOne({ _id, userId })
     .lean()
     .then(restaurant => res.render("edit", { restaurant }))
     .catch(err => console.log(err))
@@ -37,16 +41,30 @@ router.get("/:id/edit", (req, res) => {
 
 // 更新餐廳
 router.put("/:id", (req, res) => {
-  const { id } = req.params
-  Restaurant.findByIdAndUpdate(id, req.body)
-    .then(() => res.redirect(`/restaurants/${id}`))
+  const userId = req.user._id
+  const _id = req.params.id
+  return Restaurant.findOne({ _id, userId })
+    .then(restaurant => {
+      restaurant.name = name
+      restaurant.category = category
+      restaurant.image = image
+      restaurant.location = location
+      restaurant.phone = phone
+      restaurant.google_map = google_map
+      restaurant.rating = rating
+      restaurant.description = description
+      return restaurant.save()
+    })
+    .then(() => res.redirect(`/restaurants/${_id}`))
     .catch(err => console.log(err))
 })
 
 // 刪除餐廳
 router.delete('/:id', (req, res) => {
-  const { id } = req.params
-  Restaurant.findByIdAndDelete(id)
+  const userId = req.user._id
+  const _id = req.params.id
+  return Restaurant.findOne({ _id, userId })
+    .then(restaurant => restaurant.remove())
     .then(() => res.redirect('/'))
     .catch(error => console.error(error))
 })
